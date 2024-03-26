426a27362304d3b8ad952e8e66daa9ee1c9a40e7
Nargiza <r.nakosya@gmail.com>
2024-03-25 20:26:33 -0500

    added backend

--- gke/dev-cluster.tf
+++ gke/dev-cluster.tf
@@ -2,7 +2,7 @@ module "dev_cluster" {
   source = "git@github.com:nargiza0219/tf-modules.git//gke"
   service_account_id           = "gke-service-account"
   service_account_display_name = "Gke Service Account"
-  cluster_name                 = "naris-gke-cluster"
+  cluster_name                 = "dev-gke-cluster"
   cluster_location             = "us-central1-c"
   project_id                   = "iron-country-417723"
 }
