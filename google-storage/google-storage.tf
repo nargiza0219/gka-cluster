3570f2dcdc3e0c5336c1a53e44bca3c5d2c7f2e5
Nargiza <r.nakosya@gmail.com>
2024-03-25 22:15:25 -0500

    chnaged workflow

--- google-storage/google-storage.tf
+++ google-storage/google-storage.tf
@@ -1,6 +1,6 @@
 module "my-google-storage" {
   # source = "git::https://github.com/nargiza0219/tf-modules.git//google-storage?ref=main"
-  source = "../../tf-modules/google-storage"
+  source = "github.com/nargiza0219/tf-modules.git//google-storage"
   bucket-name =  "tfstate-lock-files"
   bucket-location = "US"
   bucket-storage-class = "STANDARD"
