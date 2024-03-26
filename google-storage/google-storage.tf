ebe5fd32bc20b24660d1df4c8f322d6ca5cf4d8f
Nargiza <r.nakosya@gmail.com>
2024-03-25 19:37:06 -0500

    added s3

--- google-storage/google-storage.tf
+++ google-storage/google-storage.tf
@@ -1,7 +1,9 @@
 module "my-google-storage" {
-  source = "git::https://github.com/nargiza0219/tf-modules.git//google-storage?ref=main"
+  # source = "git::https://github.com/nargiza0219/tf-modules.git//google-storage?ref=main"
+  source = "../../tf-modules/google-storage"
   bucket-name =  "tfstate-lock-files"
-  bucket-location = "us-central1-c"
+  bucket-location = "US"
   bucket-storage-class = "STANDARD"
   level-access = true
 }
+
